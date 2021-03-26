fb0e3e906aa26bbff04adf08e75adf4d6b22a9f1
apurvabanka <appu.banka@gmail.com>
2021-03-26 21:47:07 +0530

    profile screen added

--- lib/Widget/LoginDetails.dart
+++ lib/Widget/LoginDetails.dart
@@ -11,101 +11,179 @@ class LoginDetails extends StatefulWidget {
 class _LoginDetailsState extends State<LoginDetails> {
 
   APIService apiService;
+  String rid;
 
   @override
   void initState() {
     apiService = new APIService();
+    rid = "";
+    _getRiderId();
     super.initState();
   }
 
   final myController = TextEditingController();
   final myController2 = TextEditingController();
 
-  Future<void> setRiderId(int riderId) async{
+  Future<void> setRiderId(int riderId) async {
     final prefs = await SharedPreferences.getInstance();
     print(riderId);
     await prefs.setInt("rider_id", riderId);
   }
 
+  Future<Null> _getRiderId() async {
+    final prefs = await SharedPreferences.getInstance();
+    final riderId = prefs.getInt('rider_id');
+    print(riderId.toString());
+    if (riderId == null) {
+      return null;
+    }
+    setState(() {
+      rid = riderId.toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-             padding: const EdgeInsets.only(top: 50.0),
-            ),
-            Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                controller: myController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Rider ID',
-                    hintText: 'Enter your rider ID'),
+    if (rid == ""){
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                obscureText: true,
-                controller: myController2,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter password'),
-              ),
-            ),
-            FlatButton(
-              onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 60,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: FlatButton(
-                onPressed: () {
-                  apiService.riderLogin(int.parse(myController.text), myController2.text).then(
-                          (value) => setState(() {
-                            print(value);
-                            if(value == true) {
-                              print(value);
-                              setRiderId(int.parse(myController.text));
-                              Navigator.push(
-                                  context, MaterialPageRoute(builder: (_) => HomePage()));
-                            }
-                            else{
-                              print("Incorrect");
-                            }
-                          })
-                  );
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
+              Padding(
+                //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: myController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Rider ID',
+                      hintText: 'Enter your rider ID'),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 130,
-            ),
-            Text('New User? Create Account')
-          ],
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 15, bottom: 0),
+                //padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  obscureText: true,
+                  controller: myController2,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter password'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 60,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20)),
+                child: FlatButton(
+                  onPressed: () {
+                    apiService.riderLogin(
+                        int.parse(myController.text), myController2.text).then(
+                            (value) =>
+                            setState(() {
+                              print(value);
+                              if (value == true) {
+                                print(value);
+                                setRiderId(int.parse(myController.text));
+                                Navigator.push(
+                                    context, MaterialPageRoute(
+                                    builder: (_) => HomePage()));
+                              }
+                              else {
+                                Scaffold.of(context).showSnackBar(SnackBar(
+                                  content: Text("Incorrect Rider ID or password"),
+                                ));
+                              }
+                            })
+                    );
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 130,
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
   }
+    else{
+      return Scaffold(
+          body: new Container(
+            color: Colors.white,
+            child: new ListView(
+              children: <Widget>[
+                Center(
+                  child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            radius: 70.0,
+                            child: new Icon(
+                              Icons.account_circle,
+                              color: Colors.white,
+                              size: 70,
+                            ),
+                          )
+                        ],
+                      )
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                        "Hello, "+rid,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 60,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: FlatButton(
+                        onPressed: () async{
+                            final prefs = await SharedPreferences.getInstance();
+                            await prefs.setInt("rider_id", null);
+                            setState(() {
+                              rid="";
+                            });
+                        },
+                      child: Text(
+                        'Logout',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                  ),
+              ]
+              ),
+            ),
+          );
+    }
+}
 }
